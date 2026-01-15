c0e6f0bfb913c76820c9751ced161f963fb5e1ca
Samuel Almeida <samucaoliveira1995@gmail.com>
2026-01-14 21:02:41 -0300

    Set project root and update base path for production

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  root: './',
   base: process.env.NODE_ENV === "production" ? "/StyleCloneAI/" : "/",
   server: {
     port: 3000,
